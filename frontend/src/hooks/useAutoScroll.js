0712eb3b2b9a5a2809ab9ecd63745931df2d3815
Suhash-25 <suhasacharya0525@gmail.com>
2026-03-03 23:00:48 +0530

    Deleted all

--- frontend/src/hooks/useAutoScroll.js
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-
-export const useAutoScroll = (ref, deps) => {
-  useEffect(() => {
-    if (ref.current) {
-      ref.current.scrollTop = ref.current.scrollHeight;
-    }
-  }, deps);
-};
